18e28a4e87c6120f6f5e7e8eed488cf82c61fc02
kgunsik <kgunsik@nate.com>
2014-06-10 20:29:45 +0900

    성공

--- Hello.js
+++ Hello.js
@@ -2,7 +2,9 @@
  * Created by Administrator on 2014-05-26.
  */
 $(document).ready(function() {
-    $('#btn').click(function(){
-        $('.pageDiv').slideUp('slow');
+    $('#innerFade').innerfade({
+        animationtype:'fade',
+        speed:750,
+        timeout:2000
     });
-})
+});
