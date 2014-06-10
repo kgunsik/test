3017f8f0d571ddac33c5d1a6344414b5d30cfdda
kgunsik <kgunsik@nate.com>
2014-06-10 17:50:25 +0900

    성공

--- Hello.js
+++ Hello.js
@@ -1,11 +1,8 @@
 /**
  * Created by Administrator on 2014-05-26.
  */
-$(document).ready(function(){
-    $('#wrap').on('click','h1',function(){
-        var len = $('h1').length;
-        var targetHTML = $(this).html();
-        console.log(targetHTML.toString());
-        $('#wrap').append('<h1>' + len + '-' + targetHTML + '</h1>');
-    })
+$(document).ready(function() {
+    $('#btn').click(function(){
+        $('.pageDiv').slideUp('slow');
+    });
 })
